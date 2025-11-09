7b1cd69551db66b0dfd585731218da8a4171012f
PavelPyatnitskiy <pasha.1420@outlook.com>
2025-11-09 20:12:18 +0300

    [main] Correction func app.dao.base.find_all()

--- app/dao/base.py
+++ app/dao/base.py
@@ -22,6 +22,6 @@ class BaseDAO:
     @classmethod
     async def find_all(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select(cls.model).filter_by(**filter_by)
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
             result = await session.execute(query)
-            return result.scalars().all()
+            return result.mappings().all()
